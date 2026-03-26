6732a97b80ae0f2a99457e09ccdb2a9a5e6fd607
Samir3200 <samirelorf@yahoo.fr>
2026-03-26 14:24:40 +0100

    change icone

--- app/Components/NavBar.tsx
+++ app/Components/NavBar.tsx
@@ -6,6 +6,13 @@ export default function NavBar() {
   return (
     <nav className="w-full fixed top-0 left-0 z-50 bg-white border-b border-slate-200 shadow-sm">
       <div className="max-w-6xl mx-auto px-4 py-3 flex gap-6 items-center">
+        <img
+          src="https://res.cloudinary.com/dsybzvkmv/image/upload/v1774529949/reparation-symbole_efxqkn.avif"
+          alt="Logo Sami Repair"
+          width={32}
+          height={32}
+          className="mr-2"
+        />
         <Link href="/gestion" className="font-semibold text-slate-700 hover:text-blue-600 cursor-pointer">Ajouter travaux</Link>
         <Link href="/travaux" className="font-semibold text-slate-700 hover:text-blue-600 cursor-pointer">Liste des travaux</Link>
         <AuthButton />
